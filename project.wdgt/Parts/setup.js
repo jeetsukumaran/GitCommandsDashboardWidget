bde685770d9c07f832f0c0062ad34fc1329d5c81
Jeet Sukumaran <jeetsukumaran@gmail.com>
2009-11-11 19:56:14 -0600

    More content development.

--- project.wdgt/Parts/setup.js
+++ project.wdgt/Parts/setup.js
@@ -5,7 +5,7 @@ function setupParts() {
     setupParts.called = true;
     CreateText('title', { text: 'Git Commands:' });
     CreateGlassButton('done', { text: 'Done', onclick: 'showFront' });
-    CreatePopupButton('sectionMenu', { leftImageWidth: 11, options: unescape('[[%27All%27%2C %27%27%2C true]%2C %27Setup%27%2C [%27Adding/Deleting%27%2C %27AddingDeleting%27]%2C %27Committing%27%2C [%27Information%27%2C %27Info%27]%2C %27Searching%27%2C %27Sharing%27%2C %27Branching%27%2C %27Merging%27%2C %27Conflicts%27%2C %27Reverting%27%2C [%27Fix Mistakes / Undo%27%2C %27FixMistakes%27]%2C %27Plumbing%27%2C %27Stashing%27%2C %27Configuration%27%2C [%27Environment Variables%27%2C %27EnvironmentVariables%27]]'), rightImageWidth: 16, onchange: 'displayContent' });
+    CreatePopupButton('sectionMenu', { onchange: 'displayContent', options: unescape('[[%27All%27%2C %27%27%2C true]%2C %27Setup%27%2C [%27Adding/Deleting%27%2C %27AddingDeleting%27]%2C %27Committing%27%2C [%27Information%27%2C %27Info%27]%2C %27Searching%27%2C %27Sharing%27%2C %27Branching%27%2C %27Merging%27%2C %27Conflicts%27%2C %27Reverting%27%2C [%27Fix Mistakes / Undo%27%2C %27FixMistakes%27]%2C %27Plumbing%27%2C %27Stashing%27%2C %27Configuration%27%2C [%27Environment Variables%27%2C %27EnvironmentVariables%27]]'), rightImageWidth: 16, leftImageWidth: 11 });
     CreateShape('topRectangleShape', { rightImageWidth: 12, leftImageWidth: 12 });
     CreateShape('bottomRectangleShape', { rightImageWidth: 12, leftImageWidth: 12 });
     CreateScrollArea('scrollArea', { hasVerticalScrollbar: true, scrollbarDivSize: 18, autoHideScrollbars: true, scrollbarMargin: 6, spacing: 4 });
